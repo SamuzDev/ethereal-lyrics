3b7eb01b1eb07f9e6cb618d9c33a1a5bec8b7d87
samuzdev <deyverabanto@gmail.com>
2026-08-26 17:19:16 -0500

    bump: version 0.5.6

--- src/updater.py
+++ src/updater.py
@@ -6,7 +6,7 @@ import urllib.request
 import json
 from pathlib import Path
 
-VERSION = "0.5.5"
+VERSION = "0.5.6"
 REPO = "SamuzDev/ethereal-lyrics"
 GITHUB_API = f"https://api.github.com/repos/{REPO}/releases"
 
